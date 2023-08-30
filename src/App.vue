d2f3d61353d81d076c28ad6498f4fef4732a312d
ebubekirgungor <ebubekirgungor2003@gmail.com>
2023-08-30 16:04:32 +0300

    Added localStorage

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@
           <tr style="height: 10px" v-for="todo in todos" :key="todo.id">
             <td>
               <v-checkbox
-                style="margin-right: -35px; margin-bottom: -21px"
+                @click="checkbox_toggle()"
+                class="checkbox"
                 v-model="todo.done"
               ></v-checkbox>
             </td>
@@ -22,29 +23,24 @@
               ></v-btn>
             </td>
             <td style="width: 0px">
-              <v-btn @click="delete_todo(todo.id)" icon="mdi-trash-can-outline" size="small"></v-btn>
+              <v-btn
+                @click="delete_todo(todo.id)"
+                icon="mdi-trash-can-outline"
+                size="small"
+              ></v-btn>
             </td>
           </tr>
         </tbody>
       </v-table>
     </v-card>
   </div>
+  
   <v-dialog v-model="add_dialog" width="400px">
     <v-card>
-      <v-card-title
-        style="
-          margin-top: 20px;
-          pointer-events: none;
-          user-select: none;
-          text-align: center;
-        "
-        class="text-h5"
-      >
-        Add Todo
-      </v-card-title>
+      <v-card-title class="text-h5 card-title"> Add Todo </v-card-title>
       <v-row justify="center" class="ma-7">
         <v-text-field
-          style="width: 300px"
+          class="text-field"
           v-model="todo_text"
           required
         ></v-text-field>
@@ -59,20 +55,10 @@
 
   <v-dialog v-model="edit_dialog" width="400px">
     <v-card>
-      <v-card-title
-        style="
-          margin-top: 20px;
-          pointer-events: none;
-          user-select: none;
-          text-align: center;
-        "
-        class="text-h5"
-      >
-        Edit Todo
-      </v-card-title>
+      <v-card-title class="text-h5 card-title"> Edit Todo </v-card-title>
       <v-row justify="center" class="ma-7">
         <v-text-field
-          style="width: 300px"
+          class="text-field"
           v-model="update_text"
           required
         ></v-text-field>
@@ -85,18 +71,20 @@
     </v-card>
   </v-dialog>
 </template>
-
 <script>
 export default {
   data() {
     return {
-      todos: [],
+      todos: JSON.parse(localStorage.getItem("todos")) || [],
       add_dialog: false,
       edit_dialog: false,
       edit_id: 0,
     };
   },
   methods: {
+    update_localstorage() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
     add_todo() {
       if (this.todo_text === "") {
         return;
@@ -106,32 +94,53 @@ export default {
         text: this.todo_text,
         done: false,
       });
+      this.update_localstorage();
       this.todo_text = "";
     },
     edit_todo(id) {
       if (this.update_text === "") {
         return;
       }
-      this.todos.find(todo => todo.id === id).text = this.update_text;
+      this.todos.find((todo) => todo.id === id).text = this.update_text;
       this.edit_dialog = false;
+      this.update_localstorage();
     },
     delete_todo(id) {
-      this.todos.splice(this.todos.findIndex(todo => todo.id === id), 1);
+      this.todos.splice(
+        this.todos.findIndex((todo) => todo.id === id),
+        1
+      );
+      this.update_localstorage();
+    },
+    checkbox_toggle() {
+      setTimeout(() => this.update_localstorage(), 200);
     },
     open_edit_dialog(id) {
       this.edit_dialog = true;
-      this.update_text = this.todos.find(todo => todo.id === id).text;
+      this.update_text = this.todos.find((todo) => todo.id === id).text;
       this.edit_id = id;
-    }
+    },
   },
 };
 </script>
-
 <style>
 body {
   background-color: #f5f5f5;
 }
 .v-table__wrapper {
   overflow: hidden !important;
+}
+.checkbox {
+  margin-right: -35px;
+  margin-bottom: -21px;
+}
+.card-title {
+  margin-top: 20px;
+  pointer-events: none;
+  user-select: none;
+  text-align: center;
+}
+.text-field {
+  width: 300px;
 }
 </style>
